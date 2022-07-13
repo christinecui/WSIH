a7aeedc619768a24be5ff21145df2ac4f8881bf1
christinecui <37294640+christinecui@users.noreply.github.com>
2022-07-13 08:36:16 +0800

    Update P000_2_demo.py

--- P000_2_demo.py
+++ P000_2_demo.py
@@ -45,65 +45,65 @@ def train_WISH_proto(argsP):
 
     return model_P
 
-# def performance_eval(model, dataloader):
-#     # load data
-#     database_loader = dataloader['database']
-#     query_loader = dataloader['query']
-#
-#     model.eval().cuda()
-#
-#     re_BI, re_L, qu_BI, qu_L = compress(database_loader, query_loader, model)
-#
-#     ## Save
-#     _dict = {
-#         'retrieval_B': re_BI,
-#         'L_db':re_L,
-#         'val_B': qu_BI,
-#         'L_te':qu_L,
-#     }
-#     sava_path = 'hashcode/HASH_' + argsP.dataset + '_' + str(argsP.nbit) + 'bits.mat'
-#     sio.savemat(sava_path, _dict)
-#
-#     return 0
-#
-# def compress(database_loader, query_loader, model):
-#
-#     # retrieval
-#     re_BI = list([])
-#     re_L = list([])
-#     for _, (data_I, data_L, _) in enumerate(database_loader):
-#         with torch.no_grad():
-#             var_data_I = data_I.cuda()
-#             _, _, _, code_I, _ = model(var_data_I.to(torch.float))
-#         code_I = torch.sign(code_I)
-#         re_BI.extend(code_I.cpu().data.numpy())
-#         re_L.extend(data_L.cpu().data.numpy())
-#
-#     # query
-#     qu_BI = list([])
-#     qu_L = list([])
-#     for _, (data_I, data_L, _) in enumerate(query_loader):
-#         with torch.no_grad():
-#             var_data_I = data_I.cuda()
-#             _, _, _, code_I, _ = model(var_data_I.to(torch.float))
-#         code_I = torch.sign(code_I)
-#         qu_BI.extend(code_I.cpu().data.numpy())
-#         qu_L.extend(data_L.cpu().data.numpy())
-#
-#     re_BI = np.array(re_BI)
-#     re_L = np.array(re_L)
-#
-#     qu_BI = np.array(qu_BI)
-#     qu_L = np.array(qu_L)
-#
-#     return re_BI, re_L, qu_BI, qu_L
-#
-# def test_WISH(argsP, model_trained):
-#     dataloader = get_loader(argsP.batchsize, argsP.dataset)
-#     performance_eval(model_trained, dataloader)
+ def performance_eval(model, dataloader):
+     # load data
+     database_loader = dataloader['database']
+     query_loader = dataloader['query']
 
-if __name__ == '__main__':
-    model_trained = train_WISH_proto(argsP)
-    # test_WISH(argsP, model_trained)
-    print('lamda1: %.8f, lamda2: %.8f, lamda3: %.8f, lamda4: %.8f' % (argsP.lamda1, argsP.lamda2, argsP.lamda3, argsP.lamda4))
-    print("******************************************")
+     model.eval().cuda()
+
+     re_BI, re_L, qu_BI, qu_L = compress(database_loader, query_loader, model)
+
+     ## Save
+     _dict = {
+         'retrieval_B': re_BI,
+         'L_db':re_L,
+         'val_B': qu_BI,
+         'L_te':qu_L,
+     }
+     sava_path = 'hashcode/HASH_' + argsP.dataset + '_' + str(argsP.nbit) + 'bits.mat'
+     sio.savemat(sava_path, _dict)
+
+     return 0
+
+ def compress(database_loader, query_loader, model):
+
+     # retrieval
+     re_BI = list([])
+     re_L = list([])
+     for _, (data_I, data_L, _) in enumerate(database_loader):
+         with torch.no_grad():
+             var_data_I = data_I.cuda()
+             _, _, _, code_I, _ = model(var_data_I.to(torch.float))
+         code_I = torch.sign(code_I)
+         re_BI.extend(code_I.cpu().data.numpy())
+         re_L.extend(data_L.cpu().data.numpy())
+
+     # query
+     qu_BI = list([])
+     qu_L = list([])
+     for _, (data_I, data_L, _) in enumerate(query_loader):
+         with torch.no_grad():
+             var_data_I = data_I.cuda()
+             _, _, _, code_I, _ = model(var_data_I.to(torch.float))
+         code_I = torch.sign(code_I)
+         qu_BI.extend(code_I.cpu().data.numpy())
+         qu_L.extend(data_L.cpu().data.numpy())
+
+     re_BI = np.array(re_BI)
+     re_L = np.array(re_L)
+
+     qu_BI = np.array(qu_BI)
+     qu_L = np.array(qu_L)
+
+     return re_BI, re_L, qu_BI, qu_L
+
+ def test_WISH(argsP, model_trained):
+     dataloader = get_loader(argsP.batchsize, argsP.dataset)
+     performance_eval(model_trained, dataloader)
+        
+ if __name__ == '__main__':
+   model_trained = train_WISH_proto(argsP)
+   test_WISH(argsP, model_trained)
+   print('lamda1: %.8f, lamda2: %.8f, lamda3: %.8f, lamda4: %.8f' % (argsP.lamda1, argsP.lamda2, argsP.lamda3, argsP.lamda4))
+   print("******************************************")
