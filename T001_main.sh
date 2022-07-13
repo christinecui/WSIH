ea6e95a15592a6d11d57eedb4df642fc05a6ad61
christinecui <37294640+christinecui@users.noreply.github.com>
2022-07-13 08:33:09 +0800

    Update T001_main.sh

--- T001_main.sh
+++ T001_main.sh
@@ -2,25 +2,25 @@
 set -e
 
 
-for i in 16 32 64 128  16 32 64 128  16 32 64 128
+for i in 16 32 64 128
 do
 
     ##############################
     # COCO
     ##############################
-#    CUDA_VISIBLE_DEVICES=0 python P000_2_demo.py --nbit $i --dataset 'coco'
+    CUDA_VISIBLE_DEVICES=0 python P000_2_demo.py --nbit $i --dataset 'coco'
     CUDA_VISIBLE_DEVICES=0 python H000_2_demo_att.py --nbit $i --dataset 'coco'
     cd matlab &&
-    matlab -nojvm -nodesktop -r "test_save_hash_3($i, 'coco', 'T000_2'); quit;" &&
+    matlab -nojvm -nodesktop -r "test_save_hash_3($i, 'coco', 'WSIH'); quit;" &&
     cd ..
 
     ##############################
     # NUS21
     ##############################
-#    CUDA_VISIBLE_DEVICES=0 python P000_2_demo.py --nbit $i --dataset 'nus21'
+    CUDA_VISIBLE_DEVICES=0 python P000_2_demo.py --nbit $i --dataset 'nus21'
     CUDA_VISIBLE_DEVICES=0 python H000_2_demo_att.py --nbit $i --dataset 'nus21'
     cd matlab &&
-    matlab -nojvm -nodesktop -r "test_save_hash_3($i, 'nus21','T000_2'); quit;" &&
+    matlab -nojvm -nodesktop -r "test_save_hash_3($i, 'nus21','WSIH'); quit;" &&
     cd ..
 
 done
