22bd0b220f5ba29b67c3e962219f0cbb12e808a9
Hui Cui <37294640+christinecui@users.noreply.github.com>
2023-09-01 17:25:25 +0800

    Update test_save_hash_3.m

--- matlab/test_save_hash_3.m
+++ matlab/test_save_hash_3.m
@@ -1,48 +1,32 @@
 function [] = test_save_hash(nbit, dset, prefix)
     n_bits = nbit;
     dataset = dset;
-%      n_bits = 32;
-%      dataset = 'MSCOCO';
-%      prefix = 'T1111_hash';
     addpath('utils/');
     fprintf('Load hashcodes and labels of %s.\n', dataset);
     % hashcode
-    hashcode_path = sprintf('../hashcode2/HASH_%s_%dbits.mat', dataset, n_bits);
+    hashcode_path = sprintf('../hashcode/HASH_%s_%dbits.mat', dataset, n_bits);
     fprintf('End Load hashcodes and labels of %s.\n', dataset);
 
     load (hashcode_path);
     % label
     if strcmp(dataset, 'coco')
-        % 20210906 change start %
-         load('/media/zl/9CCC203CCC2012D6/ch2018/707_WISH/WISH/datasets/coco/mat/retrieval.mat', 'L_db');
-         load('/media/zl/9CCC203CCC2012D6/ch2018/707_WISH/WISH/datasets/coco/mat/test.mat', 'L_te');
-        % 20210906 change end %
+         load('./datasets/coco/retrieval.mat', 'L_db');
+         load('./datasets/coco/test.mat', 'L_te');
     elseif strcmp(dataset, 'nus21')
-        load('/media/zl/9CCC203CCC2012D6/ch2018/707_WISH/WISH/datasets/nus21/mat/retrieval.mat', 'L_db');
-        load('/media/zl/9CCC203CCC2012D6/ch2018/707_WISH/WISH/datasets/nus21/mat/test.mat', 'L_te');
+        load('./datasets/nus21/retrieval.mat', 'L_db');
+        load('./datasets/nus21/test.mat', 'L_te');
     end
     
     trn_label = double(L_db);
     tst_label = double(L_te);
     cateRetriTest = sign(double(trn_label) * double(tst_label')) == 1;
    
-    %fid = fopen('../ARGA/log_GAEH_test_time.txt', 'at');
-    %tmp_T = tic;             % time start
     B = compactbit(retrieval_B > 0);
     tB = compactbit(val_B > 0);
     hamm = hammingDist(tB, B)';
     [~, hammRetriTest] = sort(hamm, 1);
     [precision, recall, map] = fast_PR_MAP(int32(cateRetriTest), int32(hammRetriTest));
-    %test_time = toc(tmp_T); % time end
 
-%     in_file = ['---------', dataset, '----', num2str(n_bits),'----start------'];
-%     fprintf(fid,'%s', in_file);
-%     fprintf(fid,'\r\n'); 
-%     in_file = ['test time: ', num2str(test_time)];
-%     fprintf(fid,'%s', in_file);
-%     fprintf(fid,'\r\n'); 
-%     fclose(fid);   
-    
     clear hamm;
     clear cateRetriTest;
     
